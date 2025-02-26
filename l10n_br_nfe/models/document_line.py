21045196590b37a2932c6607e912a07cb090ac5e
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-02-26 10:31:53 -0300

    [FIX] l10n_br_nfe: remove benefit code

--- l10n_br_nfe/models/document_line.py
+++ l10n_br_nfe/models/document_line.py
@@ -143,7 +143,7 @@ class NFeLine(spec_models.StackedModel):
 
     # CNPJFab TODO
 
-    nfe40_cBenef = fields.Char(related="icms_tax_benefit_code")
+    nfe40_cBenef = fields.Char()
 
     # TODO em uma importação de XML deve considerar esse campo na busca do
     # ncm_id
