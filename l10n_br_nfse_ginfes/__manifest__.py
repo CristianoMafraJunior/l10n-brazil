0153dba905f9a676abef251d6541c7304409c4b3
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-08-31 22:44:21 -0300

    [WIP] DONT MERGE

--- l10n_br_nfse_ginfes/__manifest__.py
+++ l10n_br_nfse_ginfes/__manifest__.py
@@ -14,9 +14,9 @@
     "external_dependencies": {
         "python": [
             "erpbrasil.edoc>=2.5.2",
-            "erpbrasil.assinatura>=1.7.0",
+            "erpbrasil.assinatura<=1.7.0",
             "erpbrasil.transmissao>=1.1.0",
-            "erpbrasil.base>=2.3.0",
+            "erpbrasil.base<=2.3.0",
             "nfselib.ginfes",
         ],
     },
