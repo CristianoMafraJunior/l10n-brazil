7efe49a96211fde1a3c82f48c93b298de87569f5
Antônio Neto <neto@engenere.one>
2025-11-26 22:34:07 -0300

    WIP: ind final in fiscal lines

--- l10n_br_account/models/account_move.py
+++ l10n_br_account/models/account_move.py
@@ -124,6 +124,13 @@ class AccountMove(models.Model):
             self._onchange_ind_final()
         return res
 
+    @api.onchange("ind_final")
+    def _onchange_ind_final(self):
+        for move in self:
+            for line in move.invoice_line_ids:
+                if line.ind_final != move.ind_final:
+                    line.ind_final = move.ind_final
+
     @api.constrains("fiscal_document_id", "document_type_id")
     def _check_fiscal_document_type(self):
         for rec in self:
