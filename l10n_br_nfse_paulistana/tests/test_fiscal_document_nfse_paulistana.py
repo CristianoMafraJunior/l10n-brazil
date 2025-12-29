a80d5cec311d3ddb874fa8409dd42a129df19b8d
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-12-29 14:20:05 -0300

    [IMP] l10n_br_nfse_paulistana: paulistana reforma tributaria

--- l10n_br_nfse_paulistana/tests/test_fiscal_document_nfse_paulistana.py
+++ l10n_br_nfse_paulistana/tests/test_fiscal_document_nfse_paulistana.py
@@ -183,6 +183,22 @@ class TestFiscalDocumentNFSePaulistana(TestFiscalDocumentNFSeCommon):
         self.nfse_same_state.rps_number = "50"
         self.nfse_same_state.document_number = "50"
 
+        nbs = self.env.ref("l10n_br_fiscal.nbs_124043300")
+        cbs_cst = self.env.ref("l10n_br_fiscal.cst_icms_00")
+        operation_indicator = self.env.ref("l10n_br_fiscal.operation_indicator_030101")
+
+        self.nfse_same_state.rps_number = "50"
+        self.nfse_same_state.document_number = "50"
+
+        for line in self.nfse_same_state.fiscal_line_ids:
+            line.write(
+                {
+                    "nbs_id": nbs.id,
+                    "operation_indicator_id": operation_indicator.id,
+                    "cbs_cst_id": cbs_cst.id,
+                }
+            )
+
         with patch(
             "odoo.addons.l10n_br_nfse.models.document.Document.make_pdf",
             return_value=None,
